55e0670ae79924934fb28be02c7fce0eb8dba065
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-03-04 19:09:59 +0100

    Added database

--- app/src/main/java/com/example/journal/EntryDatabase.java
+++ app/src/main/java/com/example/journal/EntryDatabase.java
@@ -5,17 +5,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class EntryDatabase extends SQLiteOpenHelper {
+    private static final String TABLE_NAME = "entries";
+
     public EntryDatabase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createTable = "CREATE TABLE TABLE_NAME (ID INTEGER PRIMARY KEY, TITLE TEXT, CONTENT TEXT, MOOD INTEGER, TIMESTAMP TEXT)";
+        db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
